a2d946d46effde07e9e4ab86d01c1b369a717c93
Berkermznl <berkegaming06@gmail.com>
2022-04-09 22:06:22 +0300

    uploading data

--- data_collection.py
+++ data_collection.py
@@ -10,6 +10,6 @@ import pandas as pd
 
 path = "C:/Users/brmzn/Downloads/Compressed/chromedriver_win32_3/chromedriver.exe"
 
-df = gs.get_jobs("data scientist", 15, False, path, 15)
+df = gs.get_jobs("data scientist", 1000, False, path, 15)
 
-df
+df.to_csv("glassdoor_jobs.csv", index=False)
